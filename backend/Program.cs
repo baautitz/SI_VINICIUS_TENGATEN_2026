1d21950c172d406962145acbf04c95095d18fe2e
Vinicius Tengaten <vinitengaten@gmail.com>
2026-05-12 22:18:12 -0300

    feat(infra): update Dockerfile and project files for improved hot-reload and output path management

--- backend/Program.cs
+++ backend/Program.cs
@@ -20,8 +20,11 @@ public class Program
                 config.Path = string.Empty;
             });
         }
+        else
+        {
+            app.UseHttpsRedirection();
+        }
 
-        app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
 
