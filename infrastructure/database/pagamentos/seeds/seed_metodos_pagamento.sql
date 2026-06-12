95fc0c53a7149a2766f723226a725811a42fc5bc
Vinicius Tengaten <vinitengaten@gmail.com>
2026-06-11 21:20:16 -0300

    seed(db): convert payment methods to uppercase and use DO UPDATE

--- infrastructure/database/pagamentos/seeds/seed_metodos_pagamento.sql
+++ infrastructure/database/pagamentos/seeds/seed_metodos_pagamento.sql
@@ -3,14 +3,16 @@ SET search_path TO projeto_sistemas;
 
 INSERT INTO metodos_pagamento (codigo, descricao, ativo)
 VALUES
-  ('01', 'Dinheiro', TRUE),
-  ('02', 'Cheque', TRUE),
-  ('03', 'Cartão de Crédito', TRUE),
-  ('04', 'Cartão de Débito', TRUE),
-  ('05', 'Crédito Loja', TRUE),
-  ('15', 'Boleto', TRUE),
-  ('16', 'Depósito Bancário', TRUE),
-  ('17', 'Pix', TRUE),
-  ('90', 'Sem Pagamento', TRUE),
-  ('99', 'Outras', TRUE)
-ON CONFLICT (codigo) DO NOTHING;
+  ('01', 'DINHEIRO', TRUE),
+  ('02', 'CHEQUE', TRUE),
+  ('03', 'CARTÃO DE CRÉDITO', TRUE),
+  ('04', 'CARTÃO DE DÉBITO', TRUE),
+  ('05', 'CRÉDITO LOJA', TRUE),
+  ('15', 'BOLETO', TRUE),
+  ('16', 'DEPÓSITO BANCÁRIO', TRUE),
+  ('17', 'PIX', TRUE),
+  ('90', 'SEM PAGAMENTO', TRUE),
+  ('99', 'OUTRAS', TRUE)
+ON CONFLICT (codigo) DO UPDATE SET
+  descricao = EXCLUDED.descricao,
+  ativo = EXCLUDED.ativo;
